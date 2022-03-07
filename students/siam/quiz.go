6a47c6480cd702ea70fa12e70167bc9643eb56d7
Siam Chowdhury <siamachy@gmail.com>
2022-03-06 23:08:29 -0500

    Parse command line for file path to question file

--- students/siam/quiz.go
+++ students/siam/quiz.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main() {
+	filePathPtr := flag.String("csv", "./problems.csv", "Path to the quiz problem set.")
+
+	flag.Parse()
+
+	fmt.Println("File Path: ", *filePathPtr)
 
 }
